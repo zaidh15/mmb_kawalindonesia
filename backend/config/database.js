63958b218e54eb574a13ffeace0508163fd91d80
zaidh15 <zaidhilmi15@gmail.com>
2023-11-19 13:17:23 +0700

    re- init, new template using react js from core ui

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     "host" : "localhost",  
     "port" : "3306", 
     "user" : "root", 
-    "password" : "root",   
+    "password" : "",   
     "database" : "kawal_indonesia", 
     "waitForConnections" : true,
     "connectionLimit" : 10,
