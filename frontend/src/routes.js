c16a3e4da987a65a881cad61f3068939f535a6c2
mazzennn <mazzennn>
2023-12-04 07:50:46 +0700

    Mengganti ListForm

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,7 +4,7 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 
 //Manajemen
 const KawalIndonesia = React.lazy(() => import('./views/kawalindonesia/Project'))
-const FormKawal= React.lazy(() => import('./views/kawalindonesia/FormKawal'))
+const ListForm= React.lazy(() => import('./views/kawalindonesia/ListForm'))
 const TipeUser= React.lazy(() => import('./views/kawalindonesia/TipeUser'))
 
 
@@ -13,7 +13,7 @@ const routes = [
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/manajemen', name: 'Manajemen', element: KawalIndonesia, exact: true },
   { path: '/kawalindonesia/project', name: 'Kawal Indonesia', element: KawalIndonesia },
-  { path: '/kawalindonesia/form', name: 'Form', element: FormKawal },
+  { path: '/kawalindonesia/form', name: 'Form', element: ListForm },
   { path: '/kawalindonesia/tipeuser', name: 'Tipe User', element: TipeUser },
   { path: '/kawalindonesia/project/tipe-user/:id', name: 'Tipe User', element: TipeUser },
 ]
--- frontend/src/views/kawalindonesia/ListForm.js
+++ frontend/src/views/kawalindonesia/ListForm.js
@@ -1,100 +1,165 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { 
-    CCol,
-    CRow,
-    CButton,
-    CLink,
-    CTable,
- } from '@coreui/react'
+  CTable, 
+  CForm, 
+  CFormInput, 
+  CButton, 
+  CRow,
+  CCol,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle,
+  CLink,
+  CDropdown,
+  CDropdownMenu,
+  CDropdownItem,
+  CDropdownToggle,
 
-const ListForm = () => {
-    const columns = [
-        {
-          key: 'id',
-          label: 'No',
-          _props: { scope: 'col' },
-        },
-        {
-          key: 'nama_form',
-          label: 'Nama Form',
-          _props: { scope: 'col' },
-        },
-        {
-          key: 'kode_form',
-          label: 'Kode Form',
-          _props: { scope: 'col' },
-        },
-        {
-          key: 'deskripsi',
-          label: 'Deskripsi',
-          _props: { scope: 'col' },
-        },
-        {
-          key: 'pembuat_form',
-          label: 'Pembuat Form',
-          _props: { scope: 'col' },
-        },
-        {
-          key: 'aktif_nonaktif',
-          label: 'Aktif/Non Aktif',
-          _props: { scope: 'col' },
-        },
-        {
-          key: 'tanggal_pembuatan',
-          label: 'Tanggal Pembuatan',
-          _props: { scope: 'col' },
-        },
-        {
-          key: 'masa_berlaku',
-          label: 'Masa berlaku sampai dengan',
-          _props: { scope: 'col' },
-        },
-    ]
-
-    const items = [
-        {
-          id: 1,
-          nama_form: (
-            <CLink href='#'>Form Data Saksi</CLink>
-          ),
-          kode_form: 'DS',
-          deskripsi: 'Pendataan saksi',
-          pembuat_form: 'superadmin',
-          aktif_nonaktif: 'aktif',
-          tanggal_pembuatan: '23 September 2022',
-          masa_berlaku: '23 September 2022',
-          _cellProps: { id: { scope: 'row' } },
-        },
-        {
-          id: 2,
-          nama_form: (
-            <CLink href='#'>Form kanvasing</CLink>
-          ),
-          kode_form: 'Kanvasing-1',
-          deskripsi: 'Kanvasing 1',
-          pembuat_form: 'superadmin',
-          aktif_nonaktif: 'aktif',
-          tanggal_pembuatan: '23 Desember 2022',
-          masa_berlaku: '23 Desember 2022',
-          _cellProps: { id: { scope: 'row' } },
-        },
-      ]
+} from '@coreui/react'
 
 
-    return (
+import axios from 'axios'
+import FormListForm from "./FormListForm";
+import { format } from 'date-fns';
+import { id } from 'date-fns/locale';
+
+const FormKawal = () => {
+  const [data, setData] = useState([])
+  const [visible, setVisible] = useState(false)
+
+  const closeModal = () => {
+    setVisible(false)
+  }
+
+  const openModal = () => {
+    setVisible(true)
+  }
+
+  const getAllForm = () => {
+    axios.get("http://localhost:5005/api/forms").then((response) => {
+      setData(response.data.data)
+    })
+  }
+
+  const saveNewForm = (newData) => {
+    axios.post("http://localhost:5005/api/form", newData).then((response) => {
+      console.log(response)
+      setVisible(false)
+      getAllForm()
+    })
+  }
+
+  useEffect(() => {
+    return getAllForm()
+  },[])
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return format(date, 'dd MMMM yyyy', { locale: id });
+};
+
+
+  const columns = [
+    {
+      key: 'id',
+      label: 'No',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'nama_form',
+      label: 'Nama Form',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'kode_form',
+      label: 'Kode Form',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'deskripsi',
+      label: 'Deskripsi',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'pembuat_form',
+      label: 'Pembuat Form',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'status',
+      label: 'Status',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'tanggal_buat',
+      label: 'Tanggal Pembuatan',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'tanggal_berlaku',
+      label: 'Berlaku Sampai',
+      _props: { scope: 'col' },
+    },
+  ]
+
+  const items = data.map((item, index) => ({
+    id: index+1, 
+    nama_form: (
+      <CLink href={`../kawalindonesia/tipe-user/${item.id}`}>{item.nama_form}</CLink>
+    ),
+    kode_form: item.kode_form,
+    deskripsi: item.deskripsi,
+    pembuat_form: item.pembuat_form,
+    status: (
+        item.status === 'aktif' ? 'Aktif' : 'Non-aktif'
+      ),
+    tanggal_buat: formatDate(item.tanggal_buat),
+    tanggal_berlaku: formatDate(item.tanggal_berlaku),
+  }))
+
+  return (
     <>
-        <CRow>
-            <CCol>
-            <h5>List Form</h5>
-            </CCol>
-            <CCol xs lg={3}>
-                <CButton color="primary">Tambah Tipe User</CButton>
-            </CCol>
-        </CRow>
-        <br/>
-        <CTable columns={columns} items={items} striped/>
+    <h2>List Form</h2>
+    <CRow>
+      <CCol>
+        <CForm>
+          <CFormInput
+            type="email"
+            id="exampleFormControlInput1"
+            placeholder="Search"
+            aria-describedby="exampleFormControlInputHelpInline"
+          />
+        </CForm>
+      </CCol>
+      <CCol s lg={3}>
+        <CButton color="primary" onClick={openModal}>+ Tambah Form</CButton>
+      </CCol>
+    </CRow>
+    <br/>
+    <CTable columns={columns} items={items} striped/>
+
+    <CModal
+        size='lg'
+        alignment="center"
+        visible={visible}
+        onClose={() => setVisible(false)}
+        aria-labelledby="VerticallyCenteredExample"
+        >
+        <CModalHeader>
+            <CModalTitle id="VerticallyCenteredExample">Tambah Form</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <FormListForm 
+          onSimpanClick={saveNewForm} 
+          onCancelClick={() => setVisible(false)}
+          />
+        </CModalBody>
+        </CModal>
     </>
-    )
+  )
 }
 
-export default ListForm
+
+export default FormKawal
